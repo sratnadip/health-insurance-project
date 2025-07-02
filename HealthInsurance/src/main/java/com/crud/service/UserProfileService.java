5bba19f08984a7acf427dbe6bef4fbb345fb11c7
Samadhan Bhoi <137138845+SamBhoi@users.noreply.github.com>
2025-07-02 12:55:34 +0530

    UserProfileService
    UserProfileService

---
+++ HealthInsurance/src/main/java/com/crud/service/UserProfileService.java
@@ -0,0 +1,21 @@
+package com.crud.service;
+
+
+import com.crud.entity.UserProfile;
+
+import java.util.List;
+import java.util.Optional;
+public interface UserProfileService {
+    UserProfile createUserProfile(UserProfile userProfile);
+
+    Optional<UserProfile> getUserProfileById(Long id);
+
+    List<UserProfile> getAllUserProfiles();
+
+    UserProfile updateUserProfile(Long id, UserProfile updatedProfile);
+
+    void deleteUserProfile(Long id);
+
+
+
+}
