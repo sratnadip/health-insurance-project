9d92d53f041545931bd12e710ed123874c6c69ee
pcnikam <pcnikam3@gmail.com>
2025-10-13 15:15:25 +0530

    update admin policy

--- src/pages/Admin/AdminNavbar.js
+++ src/pages/Admin/AdminNavbar.js
@@ -32,7 +32,7 @@ const styles = {
   navbar: {
     width: "100%",
     height: "60px",
-    background: "linear-gradient(90deg, #9333ea, #ec4899)", // purple → pink
+    background: "linear-gradient(90deg, #2515cbff, #1f22e7ff)", // purple → pink
     color: "#fff",
     display: "flex",
     alignItems: "center",
